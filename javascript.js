3ee979616ea373ed540f57b7ef51404cedcf989f
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-01-26 14:08:07 +0100

    fix overwriting bitween O and X of the two players

--- javascript.js
+++ javascript.js
@@ -19,33 +19,68 @@ format_array(board);
 //m[2][4] = 8;  // last row, last column
 
 
+/*const cell = (function choose_cell (){
+    const cell_input  = window.prompt("enter a valid cell in the gameboeard matrix");
+    //console.log(cell_input);
+    
+    row = (''+cell_input)[0];
+    column = (''+cell_input)[1];
+    const array = [row, column];
+    return {array};
+})();*/
+
+
+
 function Player (name, symbol){
     this.name = name;
     this.symbol = symbol;
     this.now = function (){
         console.log("hello")
-    }
-    this.play = function () {
-        var cell_input  = window.prompt("enter a valid cell in the gameboeard matrix");
-        console.log(cell_input);
-        var row = (''+cell_input)[0];
-        var column = (''+cell_input)[1];
-        if (board[row][column] != "X" || board[row][column] != "O" ) {
-            board[row][column] = this.symbol;
-        } else {
+    } 
+    /*this.play = function () {
+        let row_column = choose_cell();
+
+        var row = row_column[0];
+        var column = row_column[1];
+
+        if ( board[row][column] != "e"){
             console.log ("choose another cell");
         }
+        board[row][column] = this.symbol;
         format_array(board);
-    };
+    };*/
 }
 
-/*function choose_cell (){
-    var cell_input  = window.prompt("enter a valid cell in the gameboeard matrix");
+function choose_cell (){
+    const cell_input = window.prompt("enter a valid cell in the gameboeard matrix");
     console.log(cell_input);
-    var row = (''+cell_input)[0];
-    var column = (''+cell_input)[1];
-    return row, column;
-}*/
+    const row = (''+cell_input)[0];
+    const column = (''+cell_input)[1];
+    return [row, column];
+}
+
+const selected_cases = [];
+
+function play (Symbol) { 
+    let n = 0; 
+    let row_column = choose_cell();
+    let el = (row_column[0]+""+row_column[1])
+    console.log(el);
+    while ( selected_cases.includes(el)){
+        console.log ("choose another cell");
+        row_column = choose_cell();
+        el = (row_column[0]+""+row_column[1]);
+    }
+
+    if (!selected_cases.includes(el)) {
+        board[row_column[0]][row_column[1]] = Symbol;
+        format_array(board);
+        selected_cases.push(el);
+        console.log(selected_cases);
+    }  
+}
+
+//board[row_column[0]][row_column[1]] == "X" || board[row_column[0]][row_column[1]] == "O"
 
 function check_board_game (playerSymbol) {
     fixed_row_inc_column(playerSymbol);
@@ -100,9 +135,9 @@ const player2 = new Player("player2", "O");
 //player1.play();
 bool = false;
 while(!bool){
-    player1.play();
+    play(player1.symbol);
     check_board_game(player1.symbol);
-    player2.play();
+    play(player2.symbol);
     check_board_game(player2.symbol);
 }
 
