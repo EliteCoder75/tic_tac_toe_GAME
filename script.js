6e6a71f79e0d43a69898b215bded7cbf7c7b02c5
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-01-27 23:36:44 +0100

    link old working game in console to the frontend

--- script.js
+++ script.js
@@ -20,78 +20,12 @@ function Player (name, symbol){
 
 
 
-const inp = document.querySelectorAll("select.inp");
-
-if (inp) {
-    
-    inp.forEach((bt) => {
-        bt.addEventListener("change", (event) => {
-            bt.setAttribute("disabled", "disabled");
-            const symbol = event.target.value;
-            const coor = event.target.name;
-            //play(symbol, coor);
-        });
-    });  
-    
-}       
-
-
-
-
-/////////////////connect the front with js
-
-
-
-// function that allows to select a cell
-/*function choose_cell (){
-    const cell_input = window.prompt("enter a valid cell in the gameboeard matrix");
-    console.log(cell_input);
-    const row = (''+cell_input)[0];
-    const column = (''+cell_input)[1];
-    return [row, column];
-}*/
-
-
-
-const selected_cases = [];
-
-
-//function that allow the user to play one shot
-/*function play (Symbol) { 
-    let n = 0; 
-    let row_column = choose_cell();
-    let el = (row_column[0]+""+row_column[1])
-    console.log(el);
-    while ( selected_cases.includes(el)){
-        console.log ("choose another cell");
-        row_column = choose_cell();
-        el = (row_column[0]+""+row_column[1]);
-    }
-
-    if (!selected_cases.includes(el)) {
-        board[row_column[0]][row_column[1]] = Symbol;
-        format_array(board);
-        selected_cases.push(el);
-        console.log(selected_cases);
-    }  
-}*/ 
-
 function play (Symbol, coord) { 
-    let n = 0; 
-   // let row_column = choose_cell();
     console.log(coord);
-    while ( selected_cases.includes(coord)){
-        console.log ("choose another cell");
-        row_column = choose_cell();
-        el = (row_column[0]+""+row_column[1]);
-    }
-
-    if (!selected_cases.includes(el)) {
-        board[row_column[0]][row_column[1]] = Symbol;
-        format_array(board);
-        selected_cases.push(el);
-        console.log(selected_cases);
-    }  
+    
+    board[row_column[0]][row_column[1]] = Symbol;
+    format_array(board);
+           
 }
 
 /* the winner needs to satisfy one of the three conditions one complete column or row or diagonal of same symbols
@@ -152,29 +86,30 @@ function inc_row_inc_column (playerSymbol){
 const player1 = new Player("player1", "X");
 const player2 = new Player("player2", "O");
 
-
-function game(){
-    bool1 = false;
-    bool2 = false;
-    while(!bool1 && !bool2) {
-        play(player1.symbol);
-        bool1 = check_board_game(player1.symbol);
-        console.log("bool1 "+ bool1);
-        if (bool1 == true) {
-            alert("player 1 wins ");
-            break;
-        }
-        play(player2.symbol);
-        bool2 = check_board_game(player2.symbol);
-        console.log("bool2 "+ bool2);
-        if (bool2 == true) {
-            alert("player 2 wins ");
-            break;
-        }
-    }
+const inp = document.querySelectorAll("select.inp");
+function choose_and_select (){
+    if (inp) {
+        
+        inp.forEach((bt) => {
+            bt.addEventListener("change", (event) => {
+                bt.setAttribute("disabled", "disabled");
+                const symbol = event.target.value;
+                const coor = event.target.name;
+                bool = false;
+                board[coor[0]][coor[1]] = symbol;
+                bool = check_board_game(symbol);
+                format_array(board);
+                if (bool == true) {
+                    alert(symbol+ " wins ");
+                }
+            });
+        });  
+        
+    }  
 }
 
-game();
+choose_and_select();
+
 
 
 
